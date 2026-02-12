a9cf2900e8adfede62f37e91e83b0a72d7bcce37
micheleferri01 <micheleferri095@gmail.com>
2026-02-12 18:27:31 +0100

    fixed filters bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,44 @@ import initialMoviesList from './assets/data/movies-list.js'
 import { genreList } from './assets/data/genre-list.js';
 
 export default function App() {
+  // da la lista dei movies
   const [moviesList, setMoviesList] = useState(initialMoviesList);
+
+  // da il genre per cui filtrare la lista die Movies
   const [filteredGenre, setFilteredGenre] = useState('');
+
+  // da il titolo per cui filtrare la lista dei Movies
   const [filterMoviesTitle, setFilterMoviesTitle] = useState('');
+
+  // da la lista dei Movies filtrata
   const [filteredMoviesList, setFilteredMoviesList] = useState(moviesList);
 
-  // handles the filter list by Genre
+ 
+  // handles the filters
   useEffect(
     () => {
-      const updatedFilteredMoviesList = filteredMoviesList.filter((movie) => {
-        return movie.genre.toLowerCase().trim().includes(filteredGenre.toLowerCase().trim())});
-      setFilteredMoviesList(updatedFilteredMoviesList.length > 0 ? updatedFilteredMoviesList : moviesList);
-    }, [filteredGenre]
-  )
+      let updatedFilteredMoviesList = moviesList;
 
-  // handles the filter list by Movie Title
-  useEffect(
-    () => {
-      const updatedFilteredMoviesList = filteredMoviesList.filter((movie) => movie.title.toLowerCase().trim().includes(filterMoviesTitle.toLowerCase().trim()));
-      setFilteredMoviesList(updatedFilteredMoviesList.length > 0 ? updatedFilteredMoviesList : moviesList);
-    }, [filterMoviesTitle]
+      if(filteredGenre){
+         updatedFilteredMoviesList = updatedFilteredMoviesList.filter((movie) =>
+      movie.genre.toLowerCase().trim().includes(filteredGenre.toLowerCase().trim())
+      );
+      }
+      
+      if(filterMoviesTitle){
+        updatedFilteredMoviesList = updatedFilteredMoviesList.filter((movie) => movie.title.toLowerCase().trim().includes(filterMoviesTitle.toLowerCase().trim()));
+      }
+
+      setFilteredMoviesList(updatedFilteredMoviesList);
+    }, [filteredGenre,filterMoviesTitle, moviesList]
   )
 
   return (
     <>
       <div className='container'>
         <div className='d-flex justify-content-between align-items-center my-3'>
-          <input value={filterMoviesTitle} onChange={(e) => setFilterMoviesTitle(e.target.value)} type="text" name='movie title' id='movie-title'/>
-          <select value={filteredGenre} onChange={(e) => setFilteredGenre(e.target.value)} name='genre' id='genre'>
+          <input value={filterMoviesTitle} onChange={(e) => setFilterMoviesTitle(e.target.value)} type="text" name='movie title filter' id='filter-movie-titles' />
+          <select value={filteredGenre} onChange={(e) => setFilteredGenre(e.target.value)} name='genre filter' id='genre-filter'>
             <option value="">Seleziona</option>
             {
               genreList.map((genre, index) => <option key={index} value={genre.toLowerCase()}>{genre}</option>)
@@ -38,8 +48,10 @@ export default function App() {
 
           </select>
         </div>
-
       </div>
+
+     
+
       <div className='container'>
         <ul className='list-group'>
           {
