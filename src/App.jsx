bcf82c2b2f48124ecb845ec247851659c57160b4
micheleferri01 <micheleferri095@gmail.com>
2026-02-12 14:52:27 +0100

    aggiunta lista dei movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-
+import { useState } from 'react'
+import initialMoviesList from './assets/data/movies-list.js'
 
 export default function App() {
-
+const [moviesList, setMoviesList]= useState(initialMoviesList);
   return (
     <>
-      
+      <ul className='list-group'>
+        {
+          moviesList.map((movie, index) => (
+            <li key={index} className='list-group-item d-flex justify-content-between align-items-center'>{movie.title} <span>{movie.genre}</span></li>
+          ))
+        }
+      </ul>
     </>
   )
 }
