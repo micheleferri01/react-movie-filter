f07294e8d99778eceddd992023ee93f2974f3bf9
micheleferri01 <micheleferri095@gmail.com>
2026-02-12 15:36:38 +0100

    aggiunto filtro lista movies usando il genre

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import initialMoviesList from './assets/data/movies-list.js'
+import { genreList } from './assets/data/genre-list.js';
 
 export default function App() {
-const [moviesList, setMoviesList]= useState(initialMoviesList);
+  const [moviesList, setMoviesList] = useState(initialMoviesList);
+  const [filteredGenre, setFilteredGenre] = useState('');
+  const [filteredMoviesList, setFilteredMoviesList] = useState(moviesList)
+
+  useEffect(
+    () => {
+      const updatedFilteredMoviesList = moviesList.filter((movie) => movie.genre.toLowerCase().includes(filteredGenre.toLowerCase()));
+      setFilteredMoviesList(updatedFilteredMoviesList.length === 0? moviesList : updatedFilteredMoviesList);
+    }, [filteredGenre]
+  )
   return (
     <>
+      <div className='my-3'>
+        <select value={filteredGenre} onChange={(e)=> setFilteredGenre(e.target.value)} name='genre' id='genre'>
+          <option value="seleziona">Seleziona</option>
+          {
+            genreList.map((genre, index) => <option key={index} value = {genre.toLowerCase()}>{genre}</option>)
+          }
+
+        </select>
+      </div>
       <ul className='list-group'>
         {
-          moviesList.map((movie, index) => (
+          filteredMoviesList.map((movie, index) => (
             <li key={index} className='list-group-item d-flex justify-content-between align-items-center'>{movie.title} <span>{movie.genre}</span></li>
           ))
         }
---
+++ src/assets/data/genre-list.js
@@ -0,0 +1,1 @@
+export const genreList = ["Fantascienza", "Thriller", "Romantico", "Azione" ];
